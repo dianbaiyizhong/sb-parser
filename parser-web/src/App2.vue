46cc9c0fba6eb570b9be515c73cfd9a85dd5d7c7
nntk <1585685035@qq.com>
2024-06-24 21:38:35 +0800

    使用g6

--- parser-web/src/App2.vue
+++ parser-web/src/App2.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="container" style="width: 100vw;height: 100vh"></div>
+  <div id="main" style="width: 100vw;height: 100vh"></div>
 </template>
 
 <script>
-import G6 from '@antv/g6';
+import * as echarts from 'echarts';
 
 export default {
   name: 'App',
@@ -136,164 +136,80 @@ export default {
 
   },
   mounted() {
-    G6.registerNode('card-node', {
-      draw: function drawShape(cfg, group) {
-        const r = 2;
-        const color = '#5B8FF9';
-        const w = cfg.size[0];
-        const h = cfg.size[1];
-        const shape = group.addShape('rect', {
-          attrs: {
-            x: -w / 2,
-            y: -h / 2,
-            width: w, //200,
-            height: h, // 60
-            stroke: color,
-            radius: r,
-            fill: '#fff',
-          },
-          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
-          name: 'main-box',
-          draggable: true,
-        });
-
-        group.addShape('rect', {
-          attrs: {
-            x: -w / 2,
-            y: -h / 2,
-            width: w, //200,
-            height: h / 2, // 60
-            fill: color,
-            radius: [r, r, 0, 0],
-          },
-          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
-          name: 'title-box',
-          draggable: true,
-        });
-
-        // title text
-        group.addShape('text', {
-          attrs: {
-            textBaseline: 'top',
-            x: -w / 2 + 8,
-            y: -h / 2 + 2,
-            lineHeight: 20,
-            text: cfg.id,
-            fill: '#fff',
-          },
-          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
-          name: 'title',
-        });
-        cfg.children &&
-        group.addShape('marker', {
-          attrs: {
-            x: w / 2,
-            y: 0,
-            r: 6,
-            cursor: 'pointer',
-            symbol: cfg.collapsed ? G6.Marker.expand : G6.Marker.collapse,
-            stroke: '#666',
-            lineWidth: 1,
-            fill: '#fff',
-          },
-          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
-          name: 'collapse-icon',
-        });
-        group.addShape('text', {
-          attrs: {
-            textBaseline: 'top',
-            x: -w / 2 + 8,
-            y: -h / 2 + 24,
-            lineHeight: 20,
-            text: 'description',
-            fill: 'rgba(0,0,0, 1)',
-          },
-          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
-          name: `description`,
-        });
-        return shape;
+    this.myChart = echarts.init(document.getElementById('main'));
+    let option = {
+      title: {
+        text: 'NPM Dependencies'
       },
-      setState(name, value, item) {
-        if (name === 'collapsed') {
-          const marker = item.get('group').find((ele) => ele.get('name') === 'collapse-icon');
-          const icon = value ? G6.Marker.expand : G6.Marker.collapse;
-          marker.attr('symbol', icon);
+      animationDurationUpdate: 1500,
+      animationEasingUpdate: 'quinticInOut',
+      series: [
+        {
+          type: 'graph',
+          layout: 'force',
+          force: {
+            repulsion: 3000,
+            edgeLength: [120, 500]
+          },
+          // progressiveThreshold: 700,
+          data: this.json.nodes.map(function (node) {
+            let symbolSize = 30
+            let color = "#c78419"
+            let showLabel = false
+            if (node.type === 2) {
+              symbolSize = 100
+              color = '#19c719'
+              showLabel = true
+            }
+            if (node.type === 1) {
+              symbolSize = 50
+              color = '#1984c7'
+              showLabel = true
+            }
+            return {
+              id: node.id,
+              name: node.name,
+              symbolSize: symbolSize,
+              itemStyle: {
+                color: color
+              },
+              label: {
+                show: showLabel
+              }
+            };
+          }),
+          edges: this.json.edges.map(function (edge) {
+            return {
+              source: edge.from,
+              target: edge.to,
+            };
+          }),
+          draggable: true,
+          edgeSymbol: ['circle', 'arrow'], // 边两端的标记类型，可以是'circle', 'rect', 'roundRect', 'triangle', 'diamond', 'pin', 'arrow', 'none'
+          // emphasis: {
+          //   focus: 'adjacency',
+          //   label: {
+          //     position: 'right',
+          //     show: true
+          //   }
+          // },
+          roam: true,
+          lineStyle: {
+            width: 0.5,
+            curveness: 0.3,
+            opacity: 0.7
+          }
         }
-      },
+      ]
+    }
+    this.myChart.setOption(option);
+
+    let that = this;
+    this.myChart.on('click', function (params) {
+      // 点击事件触发后，重新设置 option 中的 focusNodeAdjacency 属性
+      // 将 params.data 设置为焦点节点
+      that.highlightRelatedNodes(params.data.id)
     });
-
-    const data = {
-      id: 'A',
-      children: [
-        {
-          id: 'A1',
-          children: [{id: 'A11'}, {id: 'A12'}, {id: 'A13'}, {id: 'A14'}],
-        },
-        {
-          id: 'A2',
-          children: [
-            {
-              id: 'A21',
-              children: [{id: 'A211'}, {id: 'A212'}],
-            },
-            {
-              id: 'A22',
-            },
-          ],
-        },
-      ],
-    };
-
-
-    const container = document.getElementById('container');
-    const width = container.scrollWidth;
-    const height = container.scrollHeight || 500;
-
-    const graph = new G6.TreeGraph({
-      container: 'container',
-      width,
-      height,
-      modes: {
-        default: ['drag-canvas'],
-      },
-      defaultNode: {
-        type: 'card-node',
-        size: [100, 40],
-      },
-      defaultEdge: {
-        type: 'cubic-horizontal',
-        style: {
-          endArrow: true,
-        },
-      },
-      layout: {
-        type: 'indented',
-        direction: 'LR',
-        dropCap: false,
-        indent: 200,
-        getHeight: () => {
-          return 60;
-        },
-      },
-    });
-
-    graph.data(data);
-    graph.render();
-    graph.fitView();
-    graph.on('node:click', (e) => {
-      if (e.target.get('name') === 'collapse-icon') {
-        e.item.getModel().collapsed = !e.item.getModel().collapsed;
-        graph.setItemState(e.item, 'collapsed', e.item.getModel().collapsed);
-        graph.layout();
-      }
-    });
-
-    if (typeof window !== 'undefined')
-      window.onresize = () => {
-        if (!graph || graph.get('destroyed')) return;
-        if (!container || !container.scrollWidth || !container.scrollHeight) return;
-        graph.changeSize(container.scrollWidth, container.scrollHeight);
-      };
 
   }
 }
